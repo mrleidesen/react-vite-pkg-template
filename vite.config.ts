b07d61082b570df2405e686ff208b2c5a1a839cc
mrleidesen <mrleidesen@outlook.com>
2024-03-12 19:03:25 +0800

    build: external styled-components

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import dts from "vite-plugin-dts";
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib']})],
+  plugins: [react(), dts({ include: ['lib'] })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -15,11 +15,11 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      formats: ['es'],
     },
     copyPublicDir: false,
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime']
-    }
+      external: ['react', 'react/jsx-runtime', 'styled-components'],
+    },
   },
 });
