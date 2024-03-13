ed6fb0af129aaf1259e9b675215b8de8fabc0f81
mrleidesen <mrleidesen@outlook.com>
2024-03-13 16:34:55 +0800

    build: 修改

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'lib/main.ts'),
       formats: ['es'],
+      fileName: 'main',
     },
     copyPublicDir: false,
     rollupOptions: {
